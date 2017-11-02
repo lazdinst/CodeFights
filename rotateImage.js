bee1c7c3efe8980d21d7aa1ddf34bde21b3f57ad
Talis Lazdins <lazdinst@gmail.com>
2017-11-01 17:40:42 -0700

    RotateImage

--- rotateImage.js
+++ rotateImage.js
@@ -1,11 +1,3 @@
-function rotateImage(a) {
-  let size = a.length;
-  let results = a.slice();
-  console.log(size, results);
-  for(let i = 0; i < )
-
-}
-
 let a = [
         [1, 2, 3], 
         [4, 5, 6], 
@@ -19,3 +11,24 @@ console.log(rotateImage(a));
 //    [8, 5, 2],
 //    [9, 6, 3]
 //    ]
+
+function rotateImage(a) {
+  let n = a.length;
+  let temp;
+  for(let row = 0; row < n / 2; row++) {
+    for(let col = 0; col < Math.floor(n/2); col++) {
+      console.log("ROW ", row, " COL ", col);
+      temp = a[row][col];
+
+      a[row][col] = a[n - 1 - col][row];
+      console.log(a);
+      a[n - 1 - col][row] = a[n - 1 - row][n - 1 - col];
+      console.log(a);
+      a[n - 1 - row][n - 1 - col] = a[col][n - 1 - row];
+      console.log(a);
+      a[col][n - 1 - row] = temp;
+      console.log(a);
+    }
+  }
+  return a;
+}
